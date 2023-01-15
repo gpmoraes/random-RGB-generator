9a5c8d64ea4bfd1590c8acc3a848314f6dc4fef9
GUILHERME MORAES <58711878+gpmoraes@users.noreply.github.com>
2023-01-15 14:35:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 "use strict";
+
 /**
- * This simple function randomly generates  
- * the values for the colors Red, Green and Blue.
- * And sets the values in the DOM.
+ * This simple function generates a random color by generating random values for the Red, Green and Blue components.
+ * The function then sets the color in the DOM by using the generated RGB and HEX values.
  */
 function generateNewColor() {
 
-  // Random generated colors
+  // Generate random values for the color components
   let red   = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue  = Math.floor(Math.random() * 256);
 
-  // Saving the RGB pattern
+  // Create the RGB color pattern
   let color = `rgb(${red},${green},${blue})`;
 
-  // conversion of the values to hexadecimal
+  // Convert the color components to hexadecimal
   red   = red.toString(16);
   green = green.toString(16);
   blue  = blue.toString(16);
 
-  // adds zero if the hex number is below 15
+  // Add a leading zero if the hex value is less than 15
   if (red.length == 1) {
     red = `0${red}`;
   }
@@ -30,17 +30,17 @@ function generateNewColor() {
     blue = `0${blue}`;
   }
 
-  // Saving the HEX pattern
+  // Create the HEX color pattern
   let colorHex = `#${red}${green}${blue}`;
 
-  // Set the new background color
+  // Set the new background color in the DOM
   document.getElementById("bc").style.backgroundColor = color;
 
-  // Set the RGB pattern
+  // Set the RGB color pattern in the DOM
   document.querySelector("#infoDec").textContent = color;
   document.getElementById("infoDec").style.backgroundColor = "white";
 
-  // Set the HEX pattern
+  // Set the HEX color pattern in the DOM
   document.querySelector("#infoHex").textContent = colorHex;
   document.getElementById("infoHex").style.backgroundColor = "white";
 }
